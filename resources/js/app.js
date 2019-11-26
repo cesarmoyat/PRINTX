16bfccf8074895f8508097c39da9a5cbc4f80535
cesarmoyat <41448057+cesarmoyat@users.noreply.github.com>
2019-11-26 03:27:59 -0600

    Add files via upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,65 @@
-var UIcontroller = (function() {
+var UIcontroller = (function () {
     
     var DOMstrings = {
-        nav_row: 'nav-row';
+        nav_bar: 'nav',
+        logoTxt1: '#logo-printx',
+        logoTxt2: '#logo-sol'
         
+    };
+    
+    var animateCSS = function (e, animation, speed, delay, infinite, callback) {
+        var node = document.querySelector(e);
+        var del = 'delay-' + delay + 's';
+        node.classList.add('animated', animation);
+        infinite ? node.classList.add('infinite') : true;
+        if (speed) {
+            node.classList.add(speed);
+        }
+        if (delay) {
+            node.classList.add(del);
+        }
+        
+        function handleAnimationEnd() {
+            
+            node.classList.remove('animated', animation, speed, del, 'infinite');
+            node.removeEventListener('animationend', handleAnimationEnd);
+            
+            if (typeof callback === 'function') {callback(); }
+        }
+        
+        node.addEventListener('animationend', handleAnimationEnd);
     };
     
     return {
         
         stickyNav: function () {
-            if (document.body.scrollTop < 15) {
-                document.querySelector(DOMstrings.nav_row).classList.remove('sticky');
+            var sticky = document.querySelector(DOMstrings.nav_bar).offsetTop + 1;
+            /*console.log(sticky);
+            console.log(window.pageYOffset);*/
+            if (window.pageYOffset <= sticky) {
+                document.querySelector(DOMstrings.nav_bar).classList.remove('sticky');
+                if (document.querySelector(DOMstrings.logoTxt1).style.display === 'none') {
+                    document.querySelector(DOMstrings.logoTxt1).style.display = 'block';
+                    animateCSS(DOMstrings.logoTxt1, 'fadeIn', 'faster', 0, 0, function () {
+                        return true;
+                    });
+                    document.querySelector(DOMstrings.logoTxt2).style.display = 'block';
+                    animateCSS(DOMstrings.logoTxt2, 'fadeIn', 'faster', 0, 0, function () {
+                        return true;
+                    });
+                }
             } else {
-                document.querySelector(DOMstrings.nav_row).classList.add('sticky');
+                document.querySelector(DOMstrings.nav_bar).classList.add('sticky');
+                /*Animation*/
+                if (document.querySelector(DOMstrings.logoTxt1).style.display === 'block') {
+                    animateCSS(DOMstrings.logoTxt1, 'fadeOut', 'faster', 0, 0, function () {
+                        document.querySelector(DOMstrings.logoTxt1).style.display = 'none';
+                    });
+                    animateCSS(DOMstrings.logoTxt2, 'fadeOut', 'faster', 0, 0, function () {
+                        document.querySelector(DOMstrings.logoTxt2).style.display = 'none';
+                    });
+                }
+                
             }
         },
         
@@ -20,19 +68,19 @@ var UIcontroller = (function() {
         }
     };
     
-});
+})();
 
-var controller = (function() {
+var controller = (function (UIctrl) {
     
-    var setupEventListeners = function(UIcntrl) {
-        var DOM = UIcntrl.getDOMstrings();
+    var setupEventListeners = function () {
+        var DOM = UIctrl.getDOMstrings();
         
         
-        document.addEventListener('scroll', UIcntrl.stickyNav);
+        document.addEventListener('scroll', UIctrl.stickyNav);
     };
     
     return {
-        init: function() {
+        init: function () {
             console.log('Application launched succesfully');
             
             
